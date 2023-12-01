5ababb1839df4cbe76bb5c4f39150ec42445c6fb
Ahmet Ege Semiz <58528905+AegisMaze@users.noreply.github.com>
2023-12-01 11:13:23 +0100

    added nick change functionality

--- main.py
+++ main.py
@@ -28,19 +28,41 @@ async def reminder(ctx, day, time, message, channel):
     with open("reminders.json", "w") as f:
         json.dump(data, f)
 
-    await ctx.send("Done!")
+    await ctx.send("Reminder Set!")
 
 @bot.command()
-@commands.has_permissions(administrator=True)
-async def clear(ctx):
+@commands.has_permissions(manage_channels=True)
+async def clear(ctx, db):
 
-    with open("reminders.json", "w") as f:
-        json.dump({"mon": {}, "tue": {}, "wed": {}, "thu": {}, "fri": {}, "sat": {}, "sun": {}}, f)
+    if db == "reminders":
+        with open("reminders.json", "w") as f:
+            json.dump({"mon": {}, "tue": {}, "wed": {}, "thu": {}, "fri": {}, "sat": {}, "sun": {}}, f)
+        await ctx.send("Reminders Cleared!")
+        return
+    
+    if db == "nicks":
+        with open("nicks.json", "w") as f:
+            json.dump({"mon": [], "tue": [], "wed": [], "thu": [], "fri": [], "sat": [], "sun": []}, f)
+        await ctx.send("Nicks Cleared!")
+        return
+    
+    await ctx.send("Unknown Database!")
 
-    await ctx.send("Done!")
+@bot.command()
+async def nick(ctx, day, nick):
+    
+    with open("nicks.json", "r") as f:
+        data = json.load(f)
+    
+    data[day].append({"member": ctx.author.id, "guild": ctx.guild.id, "nick": nick})
+
+    with open("nicks.json", "w") as f:
+        json.dump(data, f)
+
+    await ctx.send("Nick Set!")
 
 @tasks.loop(seconds=60)
-@commands.has_permissions(administrator=True)
+@commands.has_permissions(manage_channels=True)
 async def loop():
 
     with open("reminders.json", "r") as f:
@@ -50,10 +72,28 @@ async def loop():
     time = datetime.now().strftime("%H:%M")
     if time in reminders:
         content = reminders[time]
-        await bot.get_channel(int(content["channel"])).send(content["message"])
+        channel = bot.get_channel(int(content["channel"]))
+        await channel.send(content["message"])
+
+"""
+@loop.error
+async def loop_error(self, error, ctx):
+    print("caught error")
+    if isinstance(error, error.MissingPermissions):
+        print("test")
+"""
 
 @bot.listen()
 async def on_ready():
+    
+    with open("nicks.json", "r") as f:
+        data = json.load(f)
+    nicks = data[days[datetime.today().weekday()]]
+    for triple in nicks:
+        guild = bot.get_guild(triple["guild"])
+        member = await guild.fetch_member(triple["member"])
+        await member.edit(nick=triple["nick"])
+
     loop.start()
 
 bot.run(os.getenv('TOKEN'))
